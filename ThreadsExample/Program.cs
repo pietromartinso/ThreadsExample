d7151f2697dbfcf309736b687c4b73ed706369cf
Pietro Oliveira <pietromartinso@gmail.com>
2022-04-16 09:44:53 -0300

    Getting started with C# System.Threads

---
+++ ThreadsExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ThreadsExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread01 = new Thread(MetodoThread);
+            thread01.Start();
+
+            Console.Read();
+        }
+    
+        public static void MetodoThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread sendo executada.");
+            }        
+        }
+    
+    }
+}
