e899db553d92cf2b934773ef97cd247b15669547
Pietro Oliveira <pietromartinso@gmail.com>
2022-04-16 10:51:50 -0300

    Account & withdraw example with Threads

---
+++ ThreadsExample/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadsExample
+{
+    class Account
+    {
+        private int balance = 1000;
+
+        public Object locker = new Object();
+
+        Random random = new Random();
+
+        public void Withdraw()
+        {
+            lock (locker)
+            {
+                if (balance > 0)
+                {
+                    Console.WriteLine("{0} - Balance: {1}.", Thread.CurrentThread.Name, balance);
+                    balance -= 100;
+
+                    Thread.Sleep(random.Next(1, 10) * 100);
+                    Console.WriteLine("New balance: {0}.", balance);
+                } else
+                {
+                    Console.WriteLine("There is not enough balance.");
+                }
+            }
+        }
+    }
+}
